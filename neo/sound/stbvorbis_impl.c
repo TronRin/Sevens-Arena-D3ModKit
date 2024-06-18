dhewm/dhewm3

Always optimize code of some integrated libraries, even in Debug builds
the stb libs, miniz and minizip are now always optimized to speed up
performance, esp. level loading times, in debug builds.

So far the stb libs implementation were dragged into the source file
that uses them, now I created additional source files just for them so
those can be optimized while the engine source files using those libs
can remain unoptimized in debug builds.

neo/sound/stbvorbis_impl.c
@@ -1,0 +1,14 @@
+// this source file includes the implementation of stb_vorbis
+// having it in a separate source file allows optimizing it in debug builds (for faster load times)
+// without hurting the debugability of the source files it's used in
+
+// (I'm doing this instead of renaming stb_vorbis.h to stb_vorbis.c so the configuration
+//  like STB_VORBIS_BIG_ENDIAN etc can be done here in code)
+
+#include "SDL_endian.h"
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+  #define STB_VORBIS_BIG_ENDIAN
+#endif
+#define STB_VORBIS_NO_STDIO
+#define STB_VORBIS_NO_PUSHDATA_API // we're using the pulldata API
+#include "stb_vorbis.h"

TronRin/Sevens-Arena-D3ModKit

neo/sound/stbvorbis_impl.c
@@ -1,0 +1,14 @@
+// this source file includes the implementation of stb_vorbis
+// having it in a separate source file allows optimizing it in debug builds (for faster load times)
+// without hurting the debugability of the source files it's used in
+
+// (I'm doing this instead of renaming stb_vorbis.h to stb_vorbis.c so the configuration
+//  like STB_VORBIS_BIG_ENDIAN etc can be done here in code)
+
+#include "SDL_endian.h"
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+  #define STB_VORBIS_BIG_ENDIAN
+#endif
+#define STB_VORBIS_NO_STDIO
+#define STB_VORBIS_NO_PUSHDATA_API // we're using the pulldata API
+#include "stb_vorbis.c"