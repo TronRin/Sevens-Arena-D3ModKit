ac5c4ad1be18eaf9358e92e6cfe7248b6b726dec
Krispy <nikodash2020@hotmail.com>
2024-06-19 20:22:32 -0500

    Removed ZLIB from vcpkg.json
    + Fixed MayaImport versioning

--- neo/game/anim/Anim_Import.cpp
+++ neo/game/anim/Anim_Import.cpp
@@ -41,8 +41,6 @@ If you have questions concerning this license or the applicable additional terms
 
 ***********************************************************************/
 
-idCVar g_MayaImportVersion( "g_MayaImportVersion", "2019", CVAR_GAME | CVAR_INTEGER, "" );
-
 static idStr				Maya_Error;
 
 static exporterInterface_t	Maya_ConvertModel = NULL;
@@ -92,7 +90,7 @@ void idModelExport::LoadMayaDll( void ) {
 	exporterDLLEntry_t	dllEntry;
 	char				dllPath[ MAX_OSPATH ];
 
-	fileSystem->FindDLL( va( "MayaImport%d", g_MayaImportVersion.GetInteger() ), dllPath );
+	fileSystem->FindDLL( va( "MayaImport%d", MAYA_DLL_VERSION ), dllPath );
 	if ( !dllPath[ 0 ] ) {
 		return;
 	}
